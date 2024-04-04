8cfc9ccf1a99ded867f1140ba95cbf43f7f7ab50
YudhoKusbiantoro <masakan.padang1118@gmail.com>
2024-04-04 11:08:29 +0700

    Looping dengan i di mulai dari 0 hingga bertambah sampai ke n

--- Linearsearch/Linearsearch.cpp
+++ Linearsearch/Linearsearch.cpp
@@ -24,6 +24,11 @@ void input()
 	cout << "\n-------------------\n";
 	cout << "Enter array element \n";
 	cout << "---------------------\n";
+	for (i = 0; i < n; i++)
+	{
+		cout << "<" << (i + 1) << "> ";
+		cin >> arr[i];
+	}
 
 }
 
